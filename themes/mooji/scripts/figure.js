5df6c8189bfd24aa5aa9ddcceb759bf89ea6f6a8
uyeong <uyeong21c@gmail.com>
2016-08-14 13:40:23 +0900

    이미지 경로 변경, CNAME 파일 이동

--- themes/mooji/scripts/figure.js
+++ themes/mooji/scripts/figure.js
@@ -1,14 +1,14 @@
 const path = require('path');
-const _ = require('lodash');
 
 hexo.extend.tag.register('figure', function(args) {
     const source = args[0];
     const alt = args[1];
     const caption = args[2];
+    const image = this.path.replace(/(\d{4}\/)\d{2}\/\d{2}\/([\w|-]+\/?)/g, '$1$2');
 
     return `
         <figure title="${alt}">
-          <img src="${path.join('/assets/img', this.path, source)}" alt="${alt || ''}">
+          <img src="${path.join('/assets/img', image, source)}" alt="${alt || ''}">
           <figcaption>&lt;${caption || ''}&gt;</figcaption>
         </figure>
     `;
