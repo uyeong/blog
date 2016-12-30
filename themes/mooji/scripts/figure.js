76f0f92b8d089c3c0ba5796164555d2ec7a4df64
uyeong <uyeong21c@gmail.com>
2016-12-30 16:58:04 +0900

    figure 플러그인에 width 인자 추가

--- themes/mooji/scripts/figure.js
+++ themes/mooji/scripts/figure.js
@@ -4,11 +4,16 @@ hexo.extend.tag.register('figure', function(args) {
     const source = args[0];
     const alt = args[1];
     const caption = args[2];
+    const width = args[3] ? `max-width:${args[3]}` : '';
     const image = this.path.replace(/(\d{4}\/)\d{2}\/\d{2}\/([\w|-]+\/?)/g, '$1$2');
 
     return `
         <figure title="${alt}">
-          <img src="${path.join('/assets/img', image, source)}" alt="${alt || ''}">
+          <img 
+              src="${path.join('/assets/img', image, source)}" 
+              alt="${alt || ''}" 
+              style="${width}"
+          >
           <figcaption>&lt;${caption || ''}&gt;</figcaption>
         </figure>
     `;
