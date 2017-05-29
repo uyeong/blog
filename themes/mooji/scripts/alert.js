0654044dd493f4fa871d45bde8ac87bd9330e739
uyeong <uyeong21c@gmail.com>
2017-05-30 00:08:26 +0900

    Change alert plugin

--- themes/mooji/scripts/alert.js
+++ themes/mooji/scripts/alert.js
@@ -1,24 +1,24 @@
 const _ = require('lodash');
 const marked = require('marked');
-const ICON_TYPE = {
-    success: "check-circle",
-    info: "info-circle",
-    warning: "exclamation-triangle",
-    danger: "exclamation-circle"
-};
 
 hexo.extend.tag.register('alert', function(args) {
     const type = args[0];
-    const title = args[1];
-    const message = args[2] || '';
-    const icon = ICON_TYPE[type];
+    let title = args[1];
+    let message = args[2];
+
+    if (!message) {
+        message = title;
+        title = '';
+    }
 
     return `
         <div class="alert alert--${type}">
             <div class="alert__inner">
-                <div class="alert__title">
-                    <strong>${title}</strong>
-                </div>
+                ${title ? `
+                    <div class="alert__title">
+                        <strong>${title}</strong>
+                    </div>
+                ` : ''}
                 <div class="alert__body">
                     ${marked(message)}
                 </div>
