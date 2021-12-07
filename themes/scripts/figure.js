cfb7829c18c0cb0bb83e43e20193d415179d7ecf
U-Yeong Ju <uyeong21c@gmail.com>
2021-12-07 18:06:36 +0900

    Remove console.log code, escape text

--- themes/scripts/figure.js
+++ themes/scripts/figure.js
@@ -3,7 +3,6 @@ const url_for = hexo.extend.helper.get('url_for').bind(hexo);
 
 hexo.extend.tag.register('figure', function([ source, alt, caption, width ]) {
   const image = this.path.replace(/\d{4}\/\d{2}\/\d{2}\/([\w|-]+\/?)/g, '$1');
-  console.log(path.join('/images/', image, source))
   return `
     <figure title="${alt}">
       <a href="${url_for(path.join('/images/', image, source))}" target="_blank">
@@ -13,7 +12,7 @@ hexo.extend.tag.register('figure', function([ source, alt, caption, width ]) {
           style="${width ? `max-width:${width}` : ''}"
         >
       </a>
-      ${caption ? `<figcaption><${caption}></figcaption>` : ''}
+      ${caption ? `<figcaption>&lt;${caption}&gt;</figcaption>` : ''}
     </figure>
   `;
 });
